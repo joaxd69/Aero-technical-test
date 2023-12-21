84a621ac615df41c727c6acd53c710c13f5ca9bc
unknown <joakig6@gmail.com>
2023-12-21 12:26:28 -0300

    nose

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import Layout from "./pages/Layout";
-import Cars from "./pages/Cars";
-import CarsResults from "./pages/CarsResults";
+// import Layout from "./pages/Layout";
+// import Cars from "./pages/Cars";
+// import CarsResults from "./pages/CarsResults";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="*" element={<p>There's nothing here: 404</p>} />
         <Route path="/prueba2" element={<p>probando2</p>} />
-        <Route element={<Layout />} path="/">
+        {/* <Route element={<Layout />} path="/">
           <Route element={<Cars />} path="" />
           <Route
             element={<CarsResults />}
@@ -20,7 +20,7 @@ function App() {
           />
           <Route element={<p>probando</p>} path="prueba" />
           <Route path="*" element={<p>There's nothing here: 404</p>} />
-        </Route>
+        </Route> */}
       </Routes>
     </div>
   );
