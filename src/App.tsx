6efe833c2a795409909b6615789ba19e400b2c86
unknown <joakig6@gmail.com>
2023-12-21 14:56:22 -0300

    fix router bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,17 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Cars from "./pages/Cars";
+import CarsResults from "./pages/CarsResults";
 
-// import Layout from "./pages/Layout";
-// import Cars from "./pages/Cars";
-// import CarsResults from "./pages/CarsResults";
 function App() {
   return (
     <Routes>
-      {/* <Route path="*" element={<p>There's nothing here: 404</p>} /> */}
-      <Route path="/" element={<p>nada</p>} />
-      <Route path="/prueba2" element={<p>probando2</p>} />
-      <Route path="/prueba3" element={<p>probando3</p>} />
-      <Route path="/prueba4" element={<p>probando4</p>} />
-      {/* <Route element={<Layout />} path="/">
-          <Route element={<Cars />} path="" />
-          <Route
-            element={<CarsResults />}
-            errorElement={<p>error</p>}
-            path="results"
-            ErrorBoundary={() => <p>error2</p>}
-          />
-          <Route element={<p>probando</p>} path="prueba" />
-          <Route path="*" element={<p>There's nothing here: 404</p>} />
-        </Route> */}
+      <Route element={<Layout />} path="/">
+        <Route element={<Cars />} path="" />
+        <Route element={<CarsResults />} path="results" />
+        <Route path="*" element={<p>Not found</p>} />
+      </Route>
     </Routes>
   );
 }
