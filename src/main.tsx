d52af7e5b3a3fc03c14d6ef701c39151ba3152c5
unknown <joakig6@gmail.com>
2023-12-21 12:28:01 -0300

    nose

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,9 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>
+  </BrowserRouter>
 );
