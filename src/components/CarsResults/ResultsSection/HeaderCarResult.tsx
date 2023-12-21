d7b8a56ca3d21aa4d4f7b9dc93519f79b3cd7be8
unknown <joakig6@gmail.com>
2023-12-21 11:40:59 -0300

    fix warnings

--- src/components/CarsResults/ResultsSection/HeaderCarResult.tsx
+++ src/components/CarsResults/ResultsSection/HeaderCarResult.tsx
@@ -1,6 +1,6 @@
 export default function HeaderCarResult() {
   return (
-    <div className="flex  w-full justify-between ">
+    <div className="flex  w-full justify-between py-2 ">
       <section>
         <img
           src="https://th.bing.com/th/id/OIP.DOEKdZ33Mq9ZVmoyON2bIQHaDt?rs=1&pid=ImgDetMain"
--- src/components/Generals/Slider.tsx
+++ src/components/Generals/Slider.tsx
@@ -9,7 +9,7 @@ export default function Slider({
   image3,
 }: SliderComponentProps) {
   const [allowNext, setAllowNext] = useState(true);
-  console.log(allowNext, image1, image2, image3, setAllowNext);
+  console.log(allowNext, image1, image2, image3, typeof setAllowNext);
   const [banner, setBanners] = useState<BannerStateProps>({
     banner1: true,
     banner2: false,
